86ff0cc79ba36f06e8fc9261179f6bb13b411437
wushuai <786234893@163.com>
2019-10-25 17:36:34 +0800

    10.25

--- app/src/main/java/com/lih/androidtestdemo/MainActivity.java
+++ app/src/main/java/com/lih/androidtestdemo/MainActivity.java
@@ -1,7 +1,8 @@
 package com.lih.androidtestdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        new Thread( new Runnable( ) {
+            @Override
+            public void run() {
+                //耗时任务，比如加载网络数据
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // 这里可以睡几秒钟，如果要放广告的话
+                        // sleep(3000);
+                        Intent intent = MainActivity.newInstance(MainActivity.this);
+                        startActivity(intent);
+//                        MainActivity.this.finish();
+                    }
+                });
+            }
+        } ).start();
     }
+
+    private static Intent newInstance(MainActivity mainActivity) {
+        return null;
+    }
+
+
 }
